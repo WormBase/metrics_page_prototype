e2d3873c28793dd2dff24531879811efb7064571
Paulo Nuin <nuin@genedrift.org>
2021-01-13 20:55:57 -0700

    initial commit

---
+++ python/get_data.py
@@ -0,0 +1,54 @@
+# Paulo Nuin Oct 2020
+
+import json
+import queries
+import time
+from intermine.webservice import Service
+
+query_title = {
+    'query_01': 'All C. elegans genes',
+    'query_02': 'Protein coding genes',
+    'query_03': 'All phenotypes',
+    'query_04': 'All C. elegans strains',
+    'query_05': 'All mutated alleles',
+    'query_06': 'All GO terms',
+    'query_07': 'C. elegans genes with GO annotation'
+}
+
+section = {
+        'query_01': 'section_01',
+        'query_02': 'section_01',
+        'query_03': 'section_02',
+        'query_04': 'section_03',
+        'query_05': 'section_04',
+        'query_06': 'section_05',
+        'query_07': 'section_05',
+}
+
+
+def run_queries():
+    service = Service('http://intermine.wormbase.org/tools/wormmine/service')
+
+    for x in dir(queries):
+        item = getattr(queries, x)
+        if callable(item):
+            if not item.__name__ in ['assert_result', 'Service', 'assert_greater', 'save_txt_file']:
+                time.sleep(1)
+                yield x, item()
+
+
+
+if __name__ == '__main__':
+
+    print('Running')
+    all_queries = run_queries()
+    results = {}
+    for q in all_queries:
+        print(q)
+        results[q[0]] = {'value': q[1], 'title': query_title[q[0]], 'section': section[q[0]]}
+
+    print(results)
+    output_json = open('../results.json', 'w')
+    json.dump(results, output_json)
+    output_json.close()
+
---
+++ python/queries.py
@@ -0,0 +1,91 @@
+# Paulo Nuin Oct 2020
+
+
+from intermine.webservice import Service
+service = Service("http://intermine.wormbase.org/tools/wormmine/service")
+
+def query_01():
+    """
+    """
+
+    # All c elegans genes
+    query = service.new_query("Gene")
+    query.add_view("primaryIdentifier", "organism.species")
+    query.add_constraint("organism.species", "=", "elegans", code = "A")
+    print(query)
+
+    return len(query.rows())
+
+
+def query_02():
+    """
+    """
+
+    # All genes with proteins, have to filter on genes
+    query = service.new_query("CDS")
+    query.add_view(
+        "gene.primaryIdentifier", "gene.secondaryIdentifier", "gene.symbol",
+        "protein.primaryIdentifier", "protein.sequence.length"
+    )
+    query.add_constraint("organism.name", "=", "Caenorhabditis elegans", code = "C")
+    print(query)
+
+    return len(query.rows())
+
+def query_03():
+    """
+    """
+
+    # All types of phenotypes
+    query = service.new_query("Phenotype")
+    query.add_view("identifier", "name")
+    print(query)
+
+    return len(query.rows())
+
+def query_04():
+    """
+    """
+
+    # All C elegans strains
+    query = service.new_query("Strain")
+    query.add_view("primaryIdentifier", "species")
+    query.add_constraint("species", "=", "Caenorhabditis elegans", code = "A")
+    print(query)
+
+    return len(query.rows())
+
+
+def query_05():
+
+    # All mutated alleles
+    query = service.new_query("Allele")
+    query.add_view("primaryIdentifier", "typeOfMutation")
+    query.add_constraint("species", "=", "Caenorhabditis elegans", code = "A")
+    query.add_constraint("typeOfMutation", "ONE OF", ["Deletion", "Insertion", "Substitution", "Tandem_duplication"], code = "B")
+    print(query)
+
+    return len(query.rows())
+
+def query_06():
+
+    # All GO terms
+    query = service.new_query("GOTerm")
+    query.add_view("identifier", "name")
+    print(query)
+
+    return len(query.rows())
+
+def query_07():
+
+    # C. elegans Genes with GO
+    query = service.new_query("Gene")
+    query.add_view("primaryIdentifier", "goAnnotation.qualifier")
+    query.add_constraint("primaryIdentifier", "IS NOT NULL", code = "A")
+    query.add_constraint("organism.species", "=", "elegans", code = "B")
+    query.outerjoin("goAnnotation")
+    print(query)
+
+    return len(query.rows())
+#     for row in query.rows():
+#         print(row["primaryIdentifier"], row["goAnnotation.qualifier"])
